5932da045d959292c68af42d67eacff9494591d2
xescuGC <xescugil@gmail.com>
2015-03-08 20:21:20 +0100

    Changed the blue color to yellow

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ module.exports = function (grunt) {
 			if (avg < 0.01 && !grunt.option('verbose')) {
 				return;
 			}
-			return [shorten(row[0]), chalk.blue(prettyMs(row[1])), chalk.blue(createBar(avg))];
+			return [shorten(row[0]), chalk.yellow(prettyMs(row[1])), chalk.yellow(createBar(avg))];
 		}).reduce(function (acc, row) {
 			if (row) {
 				acc.push(row);
